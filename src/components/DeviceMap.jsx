5adfd8b60aa71164960c2ee167c513a4e2673004
KavTV <46051618+KavTV@users.noreply.github.com>
2024-04-18 12:42:56 +0200

    Added weather station icon

--- src/components/DeviceMap.jsx
+++ src/components/DeviceMap.jsx
@@ -2,6 +2,7 @@ import 'leaflet/dist/leaflet.css';
 import React from 'react';
 import { MapContainer, Marker, TileLayer, Tooltip } from 'react-leaflet';
 import { useGetWeatherStationsQuery } from '../redux/services/apiSlice';
+import { icon } from 'leaflet';
 
 export default function DeviceMap() {
     const { data: weatherStations } = useGetWeatherStationsQuery()
@@ -11,7 +12,14 @@ export default function DeviceMap() {
             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="&copy; OpenStreetMap contributors" />
             {/* Add markers for weather stations */}
             {weatherStations && weatherStations.map(station => (
-                <Marker key={station.macAddress} position={[station.gpsLocation.latitude, station.gpsLocation.longitude]}>
+                <Marker 
+                key={station.macAddress} 
+                position={[station.gpsLocation.latitude, station.gpsLocation.longitude]}
+                icon={icon({
+                    iconUrl: "/src/assets/icons/weatherStationIcon.png",
+                    iconSize: [42,42]
+                })}
+                >
                     <Tooltip>{station.macAddress}</Tooltip>
                 </Marker>
             ))}
